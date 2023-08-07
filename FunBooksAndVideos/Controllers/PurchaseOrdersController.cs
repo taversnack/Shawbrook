5ec92d0465256a29eb25aa934e2d8f8f763132c3
Stephen Stewart <stephen.stewart@paxton-access.co.uk>
2023-08-07 08:56:40 +0100

    Added Controller and an API

---
+++ FunBooksAndVideos/Controllers/PurchaseOrdersController.cs
@@ -0,0 +1,27 @@
+﻿using FunBooksAndVideos.Models;
+using FunBooksAndVideos.Processors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunBooksAndVideos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchaseOrdersController : ControllerBase
+    {
+        private readonly PurchaseOrderProcessor _purchaseOrderProcessor;
+
+
+        public PurchaseOrdersController(PurchaseOrderProcessor purchaseOrderProcessor)
+        {
+            _purchaseOrderProcessor = purchaseOrderProcessor;
+        }
+
+
+        [HttpPost]
+        public IActionResult ProcessPurchaseOrder(PurchaseOrder purchaseOrder)
+        {
+            var processOrder = _purchaseOrderProcessor.ProcessPurchaseOrder(purchaseOrder);
+            return Ok(processOrder);
+        }
+    }
+}
