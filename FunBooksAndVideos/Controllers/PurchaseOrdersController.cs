59d00f91bb30556b0d8b5db2b294e9533c0e3b7a
Stephen Stewart <stephen.stewart@paxton-access.co.uk>
2023-08-08 08:52:31 +0100

    Updated Controller Work Complete

--- FunBooksAndVideos/Controllers/PurchaseOrdersController.cs
+++ FunBooksAndVideos/Controllers/PurchaseOrdersController.cs
@@ -1,4 +1,5 @@
-﻿using FunBooksAndVideos.Enums;
+﻿using FunBooksAndVideos.DTOs;
+using FunBooksAndVideos.Enums;
 using FunBooksAndVideos.Models;
 using FunBooksAndVideos.Processors;
 using Microsoft.AspNetCore.Mvc;
@@ -9,146 +10,47 @@ namespace FunBooksAndVideos.Controllers
     [ApiController]
     public class PurchaseOrdersController : ControllerBase
     {
-        private readonly PurchaseOrderProcessor _purchaseOrderProcessor;
-        private readonly List<Customer> _customers = new List<Customer>();
-        private readonly List<PurchaseOrder> _purchaseOrders = new List<PurchaseOrder>();
-
-
-        public PurchaseOrdersController(PurchaseOrderProcessor purchaseOrderProcessor)
-        {
-            _purchaseOrderProcessor = purchaseOrderProcessor;
-        }
+        private List<PurchaseOrder> _purchaseOrders = new List<PurchaseOrder>();
 
 
         [HttpPost]
-        public IActionResult ProcessPurchaseOrder(PurchaseOrder purchaseOrder)
+        public IActionResult CreatePurchaseOrder([FromBody] PurchaseOrderDto purchaseOrderDto)
         {
-            var processOrder = _purchaseOrderProcessor.ProcessPurchaseOrder(purchaseOrder);
-            return Ok(processOrder);
-        }
-
-
-        // POST /api/customers/{customerId}/memberships
-        [HttpPost("customers/{customerId}/memberships")]
-        public IActionResult AddMembership(int customerId, [FromBody] MembershipType membershipType)
-        {
-            var customer = _customers.FirstOrDefault(c => c.Id == customerId);
-            if (customer == null)
+            var purchaseOrder = new PurchaseOrder
             {
-                return NotFound();
-            }
+                CustomerId = purchaseOrderDto.CustomerId,
+                TotalPrice = purchaseOrderDto.TotalPrice
+            };
 
-            var membership = new Membership(membershipType) { IsActivated = false };
-            customer.Memberships.Add(membership);
-
-            return Ok(membership);
-        }
-
-
-        // PUT /api/customers/{customerId}/memberships/{membershipId}/activate
-        [HttpPut("customers/{customerId}/memberships/{membershipId}/activate")]
-        public IActionResult ActivateMembership(int customerId, int membershipId)
-        {
-            var customer = _customers.FirstOrDefault(c => c.Id == customerId);
-            if (customer == null)
-            {
-                return NotFound();
-            }
-
-            var membership = customer.Memberships.FirstOrDefault(m => m.MembershipId == membershipId);
-            if (membership == null)
-            {
-                return NotFound();
-            }
-
-            membership.IsActivated = true;
-
-            return Ok($"Membership with ID {membershipId} has been activated for customer with ID {customerId}.");
-        }
-
-        // GET /api/customers/{customerId}/orders
-        [HttpGet("customers/{customerId}/orders")]
-        public IActionResult GetOrderHistory(int customerId)
-        {
-            var customer = _customers.FirstOrDefault(c => c.Id == customerId);
-            if (customer == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(_purchaseOrders.Where(po => po.CustomerId == customerId));
-        }
-
-
-        // GET /api/purchase_orders/{orderId}
-        [HttpGet("purchase_orders/{orderId}")]
-        public IActionResult GetPurchaseOrder(int orderId)
-        {
-            var purchaseOrder = _purchaseOrders.FirstOrDefault(po => po.POId == orderId);
-            if (purchaseOrder == null)
-            {
-                return NotFound();
-            }
+            _purchaseOrders.Add(purchaseOrder);
 
             return Ok(purchaseOrder);
         }
 
-
-        // PUT /api/purchase_orders/{orderId}/items
-        [HttpPut("purchase_orders/{orderId}/items")]
-        public IActionResult AddItemsToPurchaseOrder(int orderId, [FromBody] List<PurchaseOrderItem> items)
+        [HttpPost("{orderId}/item")]
+        public IActionResult AddItemToPurchaseOrder(int orderId, [FromBody] ItemLineDto itemLineDto)
         {
-            var purchaseOrder = _purchaseOrders.FirstOrDefault(po => po.POId == orderId);
+            // Find the purchase order by orderId (This will be replaced by a data access mechanism).
+            var purchaseOrder = _purchaseOrders.FirstOrDefault(po => po.OrderId == orderId);
+
             if (purchaseOrder == null)
             {
-                return NotFound();
+                return NotFound("Purchase Order not found.");
             }
 
-            purchaseOrder.Items.AddRange(items);
-
-            return Ok($"Items added to purchase order with ID {orderId}.");
-        }
-
-
-        [HttpGet("customers/{customerId}/memberships")]
-        public IActionResult GetMemberships(int customerId)
-        {
-            var customer = _customers.FirstOrDefault(c => c.Id == customerId);
-            if (customer == null)
+            var itemLine = new ItemLine
             {
-                return NotFound();
-            }
+                OrderId = orderId,
+                ProductId = itemLineDto.ProductId,
+                MembershipId = itemLineDto.MembershipId
+            };
 
-            return Ok(customer.Memberships);
+            // In a real application, you would validate the itemLineDto data and handle any business rules here.
+
+            purchaseOrder.AddItemLine(itemLine);
+
+            return Ok(itemLine);
         }
 
-        // DELETE /api/purchase_orders/{orderId}
-        [HttpDelete("purchase_orders/{orderId}")]
-        public IActionResult CancelPurchaseOrder(int orderId)
-        {
-            var purchaseOrder = _purchaseOrders.FirstOrDefault(po => po.POId == orderId);
-            if (purchaseOrder == null)
-            {
-                return NotFound();
-            }
-
-            // Implement cancellation logic here, e.g., setting a status field to "Cancelled."
-
-            return Ok($"Purchase order with ID {orderId} has been cancelled.");
-        }
-
-
-        // GET /api/purchase_orders/{orderId}/shipping_slip
-        [HttpGet("purchase_orders/{orderId}/shipping_slip")]
-        public IActionResult GetShippingSlip(int orderId)
-        {
-            var purchaseOrder = _purchaseOrders.FirstOrDefault(po => po.POId == orderId);
-            if (purchaseOrder == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(purchaseOrder.ShippingSlip);
-        }
     }
 }
--- FunBooksAndVideos/Repository/Interface/IMembershipRepository.cs
+++ FunBooksAndVideos/Repository/Interface/IMembershipRepository.cs
@@ -6,5 +6,6 @@ namespace FunBooksAndVideos.Repository.Interface
     {
         Membership GetMembershipById(int membershipId);
         void UpdateMembershipStatus(MembershipStatus membershipStatus);
+        MembershipStatus GetMembershipStatus(int customerId, int MembershipId);
     }
 }
